07d17eba0915fe1c372eaec1b634afa14e8a08e8
sourav0457 <sourav0457@gmail.com>
2017-02-09 22:23:04 +0530

    Template added

--- music/urls.py
+++ music/urls.py
@@ -3,5 +3,8 @@ from . import views     #i.e. from current directory, import views
 
 #File contains url following the pattern "/music/*"
 urlpatterns = [
+    # /music/
     url(r'^$', views.index, name='index'),
+    #/music/album_id
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name = "detail"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse            #http response sends back html/basic webpage
 from django.conf.urls import include
+from django.template import loader
+from .models import Album
 
 # Create your views here.
+'''def index(request):
+    all_objects = Album.objects.all()
+    html = ''
+    for album in all_objects:
+        url = "/music/" + str(album.id) + "/"
+        html += '<a href = "' + url + '">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
+'''
 def index(request):
-    return HttpResponse("<h1>This is music app homepage</h1>")
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details of Album with id:" + str(album_id)+ "</h2>")
