d2ded8444db16a69f1f1e9d83deeb850884d1062
sourav0457 <sourav0457@gmail.com>
2017-02-09 23:53:57 +0530

    Templates done

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sourav~1\desktop\newfol~1\betsol~2\django~1\sample~1\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
